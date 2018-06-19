3a91d4e75350b190a5d1e4a56390477b4f2e7d32
hadyRashwan <h2rashwan@gmail.com>
2018-06-19 13:20:49 +0200

    adding product / pizza selectors

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store'
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store'
 
 import * as fromPizzas from './pizzas.reducer'
 
@@ -15,17 +15,4 @@ export const getProductState = createFeatureSelector<ProductState>('products');
 
 // pizza state
 
-export const getPizzaState = createSelector(getProductState,
-    (state:ProductState) => state.pizzas )
 
-export const getPizzasEntities = createSelector(getPizzaState,(fromPizzas.getPizzasEntities))
-
-
-
-
-export const getAllPizzas = createSelector(getPizzasEntities,(entities)=>{ 
-    return Object.keys(entities).map(id => entities[parseInt(id,10)])
-})
-
-export const getPizzasLoaded = createSelector(getPizzaState,(fromPizzas.getPizzaLoaded))
-export const getAllPizzasLoading = createSelector(getPizzaState,(fromPizzas.getPizzaLoading))
