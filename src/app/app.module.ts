4e12eb7c67a6f5ce6d5cf562cadc8a8652df62e5
hadyRashwan <h2rashwan@gmail.com>
2018-06-21 10:15:09 +0200

    adding router actions and effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
-import{reducers , CustomSerializer} from './store'
+import{reducers , effects , CustomSerializer} from './store'
 import{ StoreRouterConnectingModule , RouterStateSerializer } from '@ngrx/router-store'
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { storeFreeze } from 'ngrx-store-freeze';
+
 
 // this would be done dynamically with webpack for builds
 const environment = {
@@ -40,7 +41,7 @@ export const ROUTES: Routes = [
     BrowserAnimationsModule,
     RouterModule.forRoot(ROUTES),
     StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([]),
+    EffectsModule.forRoot(effects),
     environment.development ? StoreDevtoolsModule.instrument() : [],
     StoreRouterConnectingModule
   ],
---
+++ src/app/store/effects/index.ts
@@ -0,0 +1,6 @@
+import { Router } from '@angular/router';
+import { RouterEffects } from './router.effects';
+
+export const effects:any[] = [RouterEffects];
+
+export * from './router.effects'
